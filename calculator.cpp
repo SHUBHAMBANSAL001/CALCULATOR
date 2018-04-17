885ecfa1b70fa4f9cf3b36301c6883c5e50dff34
Shubham <500060901@stu.upes.ac.in>
2018-04-17 11:14:52 +0530

    Project calculator

---
+++ calculator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void sum();//headed by Shraddha
+void diff();//Headed by vedansh
+void div();//Headed by sri sai valli
+void mul();//Headed by Vanshika
+int main()
+{
+	int a ,b;
+	cout<<"Enter the values of the numbers";
+	cin>>a>>b;
+	
+return 0;
+}
+
