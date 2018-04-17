f4cfcfc1036196f5c902411e184037c72695934b
Your Name <you@example.com>
2018-04-17 12:01:18 +0530

    division functinality added

--- calculator.cpp
+++ calculator.cpp
@@ -4,7 +4,7 @@ void sum();//headed by Shraddha
 void diff();
             
 //Headed by vedansh
-void div();//Headed by sri sai valli
+void div(int,int);
 void mul();//Headed by Vanshika
 int main()
 {
@@ -22,5 +22,11 @@ void diff(int a,int  b)
 	else
 		c=b-a;
         cout<<"The differenc of the entered number is "<<endl<<c;
+}
+void div(int a,int b)
+{
+	int c;
+	c=a/b;
+	cout<<"The result of division is"<<c;
 }
 
