8cfff6e356c6971f323da49b3fafcfc1608244b0
saurabh <shraddhasaini99@gmail.com>
2018-04-17 11:56:52 +0530

    sum fucntionality

--- calculator.cpp
+++ calculator.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
-void sum();//headed by Shraddha
+void sum(int,int);//headed by Shraddha
 void diff();
-            
+
 //Headed by vedansh
 void div();//Headed by sri sai valli
 void mul();//Headed by Vanshika
@@ -11,7 +11,7 @@ int main()
 	int a ,b;
 	cout<<"Enter the values of the numbers";
 	cin>>a>>b;
-	
+
 return 0;
 }
 void diff(int a,int  b)
@@ -23,4 +23,9 @@ void diff(int a,int  b)
 		c=b-a;
         cout<<"The differenc of the entered number is "<<endl<<c;
 }
-
+void sum(int a , int b)
+{
+	int c;
+	c=a+b;
+	cout<<"sum of two numbers is "<<c;
+}
